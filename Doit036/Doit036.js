4b2b909f05029eca89b5e45a42e081518295e9b8
gsong <rmsdlf1206@gmail.com>
2023-10-10 18:13:52 +0900

    real finish

--- Doit036/Doit036.js
+++ Doit036/Doit036.js
@@ -5,15 +5,15 @@ let input = fs.readFileSync(filePath).toString().trim();
 solution(input);
 
 function solution(input) {
-    const DATA = input.split("-");
+    const DATA = input.split("-"); //일단 - 끼리 자르고
     let result = 0;
-    const REAL_DATA = DATA.map((v) => v.split("+").map(Number));
+    const REAL_DATA = DATA.map((v) => v.split("+").map(Number)); // 그다음에 +끼리 자르는거임
     for (let i = 0; i < REAL_DATA.length; i++) {
         REAL_DATA[i] = REAL_DATA[i].reduce((acc, now) => acc + now, 0);
-    }
-    REAL_DATA.length > 1
-        ? (result = REAL_DATA.reduce((acc, now) => acc - now, REAL_DATA[0] * 2))
-        : (result = REAL_DATA[0]);
+    } // +들 연산 해
+    REAL_DATA.length > 1 //사망연싼자
+        ? (result = REAL_DATA.reduce((acc, now) => acc - now, REAL_DATA[0] * 2)) // 이제빼주자
+        : (result = REAL_DATA[0]); // 없으면 그냥 해버리는거임
 
     console.log(result);
 }
